a7b158be9e554dcef31432a4e342647ac5118862
Emmanuel Abiodun Akinjogunla <105178231+ABIODUNTHEKING@users.noreply.github.com>
2022-10-10 02:04:56 +0100

    DONE

--- script.js
+++ script.js
@@ -1,23 +1,28 @@
 let btn = document.querySelector("#submit");
 let firstError = document.querySelector(".firstError");
 let secondError = document.querySelector(".secondError");
+let thirdError = document.querySelector(".thirdError");
 let firstInput = document.querySelector("#name");
 let secondInput = document.querySelector("#email");
+
 let pattern =  /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
 
 btn.addEventListener("click", (e)=>{
-    e.preventDefault();
+    
     if(firstInput.value == ""){
+        e.preventDefault();
         firstInput.style.border = "1px solid black"
         firstError.innerHTML = "Please fill in your name"
     }
     else if(firstInput.value !== "" && secondInput.value == ""){
+        e.preventDefault();
         firstError.style.display = "none";
         secondError.style.display = "block"
         secondError.innerHTML = "Please fill in your email"
     }
 
     else if(!(secondInput.value.match(pattern))){
+        e.preventDefault();
             secondInput.style.border = "1px solid black"
             secondError.innerHTML = "Please fill in the right email"       
     }
